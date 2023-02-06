5943fc07b724f824e160b8c6587dd0ce6e40e0d1
KamoArutyunyan456 <124582331+KamoArutyunyan456@users.noreply.github.com>
2023-02-06 13:21:31 +0100

    Add files via upload

---
+++ Even or Odd Python.py
@@ -0,0 +1,15 @@
+
+#Even or Odd Python Kata 8 kyu
+
+#Description:
+
+#Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+# Solution: 
+def even_or_odd(number):
+    if number%2:
+        return 'Odd'
+    else:
+        return 'Even'
